07c68ab76f80da0fda384517653f443a38d61487
dvub <84478450+dvub@users.noreply.github.com>
2023-04-23 16:17:35 -0700

    update index

--- src/index.js
+++ src/index.js
@@ -64,32 +64,21 @@ class RigidBody {
                 if ((result.normal.z > 0 && this.velocity.z < 0) || (result.normal.z < 0 && this.velocity.z > 0))
                     this.velocity.z = 0;
             }
+            else {
+                // elastic collision
+            }
             if (r.friction) {
                 // Ff  = U * Fn
                 // Ff = Umg
-                const frictionForce = Math.abs(this.acceleration.y * this.mass * r.friction);
-                if (__classPrivateFieldGet(this, _RigidBody_frictionApplied, "f").x === 0 && Math.abs(this.velocity.x) > 0) {
+                const frictionForce = Math.abs(this.acceleration.y * this.mass * r.friction * deltaTime);
+                if (Math.abs(this.velocity.x) > 0) {
                     if (Math.abs(this.velocity.x) >= frictionForce) {
                         this.velocity.x += (this.velocity.x > 0 ? -frictionForce : frictionForce);
                     }
                     else {
                         this.velocity.x = 0;
                     }
-                    __classPrivateFieldGet(this, _RigidBody_frictionApplied, "f").x = 1;
                 }
-                if (this.velocity.x === 0)
-                    __classPrivateFieldGet(this, _RigidBody_frictionApplied, "f").x = 0;
-                if (__classPrivateFieldGet(this, _RigidBody_frictionApplied, "f").z === 0 && Math.abs(this.velocity.z) > 0) {
-                    if (Math.abs(this.velocity.z) >= frictionForce) {
-                        this.velocity.z += (this.velocity.z > 0 ? -frictionForce : frictionForce);
-                    }
-                    else {
-                        this.velocity.z = 0;
-                    }
-                    __classPrivateFieldGet(this, _RigidBody_frictionApplied, "f").z = 1;
-                }
-                if (this.velocity.z === 0)
-                    __classPrivateFieldGet(this, _RigidBody_frictionApplied, "f").z = 0;
             }
         });
         // transform
@@ -105,9 +94,10 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 const stats = new Stats();
-const controls = new OrbitControls(camera, renderer.domElement);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.y = -5;
+camera.lookAt(new THREE.Vector3(0, 0, -10));
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 document.body.appendChild(renderer.domElement);
 document.body.appendChild(stats.dom);
@@ -115,6 +105,11 @@ const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 cube.position.z = -10;
+const material1 = new THREE.MeshLambertMaterial({ color: 0xff0000 });
+const cube1 = new THREE.Mesh(geometry, material1);
+cube1.position.z = -10;
+cube1.position.x = 3;
+cube1.position.y = -2;
 const floorGeometry = new THREE.BoxGeometry(10, 0.1, 5);
 const floorMaterial = new THREE.MeshLambertMaterial({ color: 0xff00ff });
 const floor = new THREE.Mesh(floorGeometry, floorMaterial);
@@ -127,22 +122,25 @@ wall.position.x = 2.5;
 const wall2 = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 0.1), floorMaterial);
 wall2.position.y = -3;
 wall2.position.z = -12.5;
-wall2.position.x = 0;
+wall2.position.x = 2;
 const light = new THREE.AmbientLight(0x404040); // soft white light
 const pl = new THREE.PointLight(0xFFFFFF, 1, 100);
 pl.position.set(0, 5, -10);
 // declare rigid bodies
 const cb = new RigidBody(cube, 1, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0), false);
-const fb = new RigidBody(floor, 1, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0), true);
+const cb1 = new RigidBody(cube1, 2, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0), false);
+const fb = new RigidBody(floor, 1, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0), true, 0.25);
 const wb = new RigidBody(wall, 1, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0), true);
 const wb1 = new RigidBody(wall2, 1, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0), true);
 scene.add(cube);
+scene.add(cube1);
 scene.add(floor);
 //scene.add(wall);
 //scene.add(wall2);
 scene.add(light);
 scene.add(pl);
 bodies.push(cb);
+bodies.push(cb1);
 bodies.push(fb);
 //bodies.push(wb);
 //bodies.push(wb1);
@@ -179,7 +177,7 @@ document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
     var keyCode = event.which;
     if (keyCode == 32) {
-        cb.velocity.x = -5;
+        cb.velocity.x = 5;
     }
 }
 ;
@@ -187,7 +185,7 @@ document.addEventListener("keyup", onDocumentKeyUp, false);
 function onDocumentKeyUp(event) {
     var keyCode = event.which;
     if (keyCode == 32) {
-        cb.velocity.x = 0;
+        // cb.velocity.x = 0;
     }
 }
 ;
