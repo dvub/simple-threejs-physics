357afea6766ca40aa208501fd8c10b122680b568
dvub <84478450+dvub@users.noreply.github.com>
2023-04-23 21:46:31 -0700

    update index

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ class RigidBody {
         __classPrivateFieldSet(this, _RigidBody_acceleration, acceleration, "f");
         __classPrivateFieldSet(this, _RigidBody_isStationary, isStationary, "f");
         __classPrivateFieldSet(this, _RigidBody_friction, friction, "f");
+        bodies.push(this);
     }
     // setters
     set obj(obj) { __classPrivateFieldSet(this, _RigidBody_obj, obj, "f"); }
@@ -52,6 +53,7 @@ class RigidBody {
         this.acceleration.y = -9.81;
         // v = v0 + at
         // velocity += acceleration * time
+        rk4(this.obj.position.x, __classPrivateFieldGet(this, _RigidBody_velocity, "f").x);
         this.velocity.add(__classPrivateFieldGet(this, _RigidBody_acceleration, "f").clone().multiplyScalar(this.mass * deltaTime));
         // collision response implementation, extra dynamics due to collision go here
         detectCollision(__classPrivateFieldGet(this, _RigidBody_obj, "f"), bodies, (result, r) => {
@@ -139,17 +141,13 @@ scene.add(floor);
 //scene.add(wall2);
 scene.add(light);
 scene.add(pl);
-bodies.push(cb);
-bodies.push(cb1);
-bodies.push(fb);
-//bodies.push(wb);
-//bodies.push(wb1);
 function animate() {
     requestAnimationFrame(animate);
     const deltaTime = clock.getDelta();
     for (let i = 0; i < bodies.length; i++) {
         bodies[i].update(deltaTime);
     }
+    // render, update shit
     renderer.render(scene, camera);
     stats.update();
 }
@@ -173,19 +171,38 @@ const detectCollision = (obj, bodies, onCollision) => {
         }
     }
 };
+// code adapted from: https://mtdevans.com/index.html
+// Converted from Python version: http://doswa.com/2009/01/02/fourth-order-runge-kutta-numerical-integration.html
+function rk4(x, v, a, dt) {
+    // Returns final (position, velocity) array after time dt has passed.
+    //        x: initial position
+    //        v: initial velocity
+    //        a: acceleration function a(x,v,dt) (must be callable)
+    //        dt: timestep
+    var x1 = x;
+    var v1 = v;
+    var a1 = a(x1, v1, 0);
+    var x2 = x + 0.5 * v1 * dt;
+    var v2 = v + 0.5 * a1 * dt;
+    var a2 = a(x2, v2, dt / 2);
+    var x3 = x + 0.5 * v2 * dt;
+    var v3 = v + 0.5 * a2 * dt;
+    var a3 = a(x3, v3, dt / 2);
+    var x4 = x + v3 * dt;
+    var v4 = v + a3 * dt;
+    var a4 = a(x4, v4, dt);
+    var xf = x + (dt / 6) * (v1 + 2 * v2 + 2 * v3 + v4);
+    var vf = v + (dt / 6) * (a1 + 2 * a2 + 2 * a3 + a4);
+    return {
+        position: xf,
+        velocity: vf,
+    };
+}
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
     var keyCode = event.which;
     if (keyCode == 32) {
         cb.velocity.x = 5;
-    }
-}
-;
-document.addEventListener("keyup", onDocumentKeyUp, false);
-function onDocumentKeyUp(event) {
-    var keyCode = event.which;
-    if (keyCode == 32) {
-        // cb.velocity.x = 0;
     }
 }
 ;
