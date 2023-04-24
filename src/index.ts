fb5b950dc9d7beadf43572a69796601be648d603
dvub <84478450+dvub@users.noreply.github.com>
2023-04-24 13:36:38 -0700

    refactor for vectors

--- src/index.ts
+++ src/index.ts
@@ -54,21 +54,12 @@ class RigidBody {
         // v = v0 + at
         // velocity += acceleration * time
 
-        const o = rk4(this.obj.position.x, this.#velocity.x, (x,v,dt) => {
-            return 0;
-        }, deltaTime);
-        const t= rk4(this.obj.position.y, this.#velocity.y, (x,v,dt) => {
-
-            return this.acceleration.y * this.mass;
-        }, deltaTime);
-               const y = rk4(this.obj.position.z, this.#velocity.z, (x,v,dt) => {
-
-            return 0;
+        const o = rk4(this.obj.position, this.velocity, (x,v,dt) => {
+            const g = this.acceleration.y * this.mass;
+            return new THREE.Vector3(0,g,0);
         }, deltaTime);
 
-        this.velocity.x = o.velocity;
-        this.velocity.y = t.velocity;
-        this.velocity.z = y.velocity;
+        this.velocity = o.velocity;
 
         // collision response implementation, extra dynamics due to collision go here
         detectCollision(this.#obj, bodies, (result, r) => {
@@ -240,9 +231,9 @@ const detectCollision = (
 
 // Converted from Python version: http://doswa.com/2009/01/02/fourth-order-runge-kutta-numerical-integration.html
 function rk4(
-    x: number,
-    v: number,
-    a: (_x: number, _v: number, _dt: number) => number,
+    x: THREE.Vector3,
+    v: THREE.Vector3,
+    a: (_x: THREE.Vector3, _v: THREE.Vector3, _dt: number) => THREE.Vector3,
     dt: number
 ) {
     // Returns final (position, velocity) array after time dt has passed.
@@ -250,21 +241,23 @@ function rk4(
     //        v: initial velocity
     //        a: acceleration function a(x,v,dt) (must be callable)
     //        dt: timestep
-    var x1 = x;
-    var v1 = v;
-    var a1 = a(x1, v1, 0);
+    const x1 = x.clone();
+    const v1 = v.clone();
+    const a1 = a(x1, v1, 0);
 
-    var x2 = x + 0.5 * v1 * dt;
-    var v2 = v + 0.5 * a1 * dt;
-    var a2 = a(x2, v2, dt / 2);
+    const x2 = x.clone().add(v1.clone().multiplyScalar(0.5).multiplyScalar(dt));
+    const v2 = v.clone().add(a1.clone().multiplyScalar(0.5).multiplyScalar(dt));
 
-    var x3 = x + 0.5 * v2 * dt;
-    var v3 = v + 0.5 * a2 * dt;
-    var a3 = a(x3, v3, dt / 2);
+    const a2 = a(x2, v2, dt / 2);
 
-    var x4 = x + v3 * dt;
-    var v4 = v + a3 * dt;
+    const x3 = x.clone().add(v2.clone().multiplyScalar(0.5).multiplyScalar(dt));
+    const v3 = v.clone().add(a2.clone().multiplyScalar(0.5).multiplyScalar(dt));
+    const a3 = a(x3, v3, dt / 2);
+
+    var x4 = x.clone().add(v3.multiplyScalar(dt));
+    var v4 = v.clone().add(a3.multiplyScalar(dt));
     var a4 = a(x4, v4, dt);
+
 
     var xf = x + (dt / 6) * (v1 + 2 * v2 + 2 * v3 + v4);
     var vf = v + (dt / 6) * (a1 + 2 * a2 + 2 * a3 + a4);
